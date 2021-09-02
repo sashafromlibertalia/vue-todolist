e387fe7fbd15d47a252feb2d31d8ef32ca181550
Alexander <floydreme@yandex.ru>
2021-09-02 23:26:08 +0300

    feature: version #1 of todo-item

---
+++ src/components/TodoItem/TodoItem.vue
@@ -0,0 +1,19 @@
+<template>
+    <li class="todo__item">
+        <div class="todo__checkbox-container">
+            <input class="todo__checkbox" type="checkbox" id="todo__checkbox">
+            <label class="todo__checkmark" for="todo__checkbox"></label>
+        </div>
+        <h3 class="todo__title">🍾  Купить шампанское</h3>
+    </li>
+</template>
+
+<script>
+export default {
+    name: "TodoItem"
+}
+</script>
+
+<style scoped lang="scss">
+@import "TodoItem";
+</style>
--- src/sections/TasksSection/TasksSection.vue
+++ src/sections/TasksSection/TasksSection.vue
@@ -1,14 +1,28 @@
 <template>
-<section>
-<TaskTemplate></TaskTemplate>
-</section>
+    <section>
+        <TaskTemplate></TaskTemplate>
+        <ul class="todo">
+            <TodoItem></TodoItem>
+        </ul>
+    </section>
 </template>
 
 <script>
 import TaskTemplate from "@/components/TaskTemplate/TaskTemplate";
+import TodoItem from "@/components/TodoItem/TodoItem";
+
 export default {
     name: "TasksSection",
-    components: {TaskTemplate}
+    components: {TodoItem, TaskTemplate},
+    data() {
+        return {
+            todos: [
+                '🍾  Купить шампанское',
+                '🎉  Попасть на стажировку в крупную компанию',
+                '🤝 Подписать договор'
+            ]
+        }
+    }
 }
 </script>
 
